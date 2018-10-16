20f3225fcb07ded1dff3ae1ebbcf0fd3f7ed0029
pointware <pwangkk@gmail.com>
2018-10-17 00:42:43 +0900

    props 변화에 따라 전체 셋업될 수 있도록 설정

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,51 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <EfmCategory v-model="category"
-        />
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <EfmCategory v-model="category"
+    />
+    <button @click="click">
+      change
+    </button>
+    <button @click="changeRoot">
+      change
+    </button>
+  </div>
 </template>
 
 <script>
-    import EfmCategory from './components/efm-category.vue'
+  import EfmCategory from './components/efm-category.vue'
 
-    export default {
-        name: 'app',
-        components: {
-            EfmCategory
-        },
-        data(){
-            return {
-                category: {
-                    codes: ['5','54','544'],
-                    names: [],
-                }
-            }
+  export default {
+    name: 'app',
+    components: {
+      EfmCategory
+    },
+    data() {
+      return {
+        category: {
+          codes: ['5', '54', '544'],
+          names: [],
         }
+      }
+    },
+    methods: {
+      click(){
+        this.category.codes.splice(2,1);
+      },
+      changeRoot(){
+        this.category.codes = ['3','31','312'];
+      }
     }
+  }
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/efm-category-item-base.js
+++ src/components/efm-category-item-base.js
@@ -1,102 +1,104 @@
 export default {
-    props: {
-        level: {
-            type: Number,
-            required: true
-        },
-        event: {
-            type: Object,
-            required: true
-        },
-        initCode: {
-            type: String,
-            required: false,
-            default() {
-                return null
-            }
-        },
-        initParentCode: {
-            type: String,
-            required: false,
-            default() {
-                return null
-            }
-        }
+  props: {
+    level: {
+      type: Number,
+      required: true
     },
-    data() {
-        return {
-            categories: [{
-                code: '',
-                name: '선택하세요'
-            }],
-            selectedCode: '',
-            parentCode: '',
-            isLoading: false,
-            isParentLoading: false,
-            isInit: false
-        };
-    },
-    mounted() {
-
-        if (this.level === 1 || this.isExistInitCode) {
-            this.parentCode = this.initParentCode;
-            this.setCategoryInfo();
-        }
-
-        this.event.$on('efm-category-change', this.onChangeCategory);
-        this.event.$on('efm-category-load', this.onLoad);
-    },
-    computed: {
-        isDisabled() {
-            return this.categories.length === 0 || this.isLoading || this.isParentLoading;
-        },
-        isExistInitCode() {
-            return (this.level === 1 && this.initCode) || (this.initParentCode && this.initCode);
-        }
-    },
-    methods: {
-        setCategoryInfo() {
-            this.isLoading = true;
-            this.event.$emit('efm-category-load', true, this.level);
-            this.categories = this.categories.slice(0, 1);
-            this.getCategoryInfo().then( (success) => {
-                this.categories = this.categories.concat(success);
-                if (!this.isInit && this.isExistInitCode) {
-                    this.selectedCode = this.initCode;
-                    this.isInit = true;
-                }
-                this.isLoading = false;
-                this.event.$emit('efm-category-load', false, this.level);
-            }, (reject) => {
-                //TODO: reject에 대해서 예외처리 필요
-                console.log(reject);
-            });
-        },
-        getCategoryInfo() {},
-        changeCategory(e) {
-            let selectedIndex = e.target.selectedIndex;
-            let name = selectedIndex === -1 ? '' : e.target.options[selectedIndex].text;
-            this.event.$emit('efm-category-change', this.level, this.selectedCode, name);
-        },
-        onChangeCategory(level, selectedCode) {
-            if (level + 1 === this.level) {
-                this.parentCode = selectedCode;
-                if (this.parentCode) {
-                    this.setCategoryInfo()
-                } else {
-                    this.categories = this.categories.slice(0, 1);
-                }
-            }
-            if (level < this.level) {
-                this.selectedCode = '';
-            }
-        },
-        onLoad(state, level) {
-            this.onPrevLoad();
-            if (level < this.level) {
-                this.isParentLoading = state;
-            }
-        },
-        onPrevLoad() {},
+    event: {
+      type: Object,
+      required: true
     }
+  },
+  data() {
+    return {
+      categories: [{
+        code: '',
+        name: '선택하세요'
+      }],
+      selectedCode: '',
+      parentCode: '',
+      isLoading: false,
+      isParentLoading: false
+    };
+  },
+  mounted() {
+    this.event.$on('efm-category.change', this.onChangeCategory);
+    this.event.$on('efm-category.load', this.onLoad);
+    this.event.$on('efm-category.setCode', this.setCategoryCode);
+    //this.setCategoryCode();
+
+    if(this.level === 1){
+      this.setLoading(true);
+      this.getCategoryInfo().then((success) => {
+        this.categories = this.categories.concat(success);
+        this.selectedCode = '';
+      }).finally(() => {
+        this.setLoading(false);
+      });
+    }
+  },
+  computed: {
+    isDisabled() {
+      return this.categories.length === 0 || this.isLoading || this.isParentLoading;
+    }
+  },
+  methods: {
+    setCategoryCode: function (codes) {
+      let changedParent = this.level === 1 ? '' : codes[this.level - 2];
+      let changedCode = codes.length > 0 && codes[this.level - 1] ? codes[this.level - 1] : '';
+
+      if (this.level === 1) {
+        if (this.selectedCode !== changedCode) {
+          this.selectedCode = changedCode;
+        }
+        return;
+      } else {
+        if (!changedParent || this.selectedCode === changedCode && changedParent === this.parentCode) {
+          return;
+        }
+      }
+      this.parentCode = codes[this.level - 2];
+      this.categories = this.categories.slice(0, 1);
+
+      this.setLoading(true);
+      this.getCategoryInfo().then((success) => {
+        this.categories = this.categories.concat(success);
+        this.selectedCode = changedCode;
+      }).finally(() => {
+        this.setLoading(false);
+      });
+    },
+    setLoading(set){
+      if(set){
+        this.event.$emit('efm-category.load', true, this.level);
+        this.isLoading = true;
+      }else{
+        this.isLoading = false;
+        this.event.$emit('efm-category.load', false, this.level);
+      }
+    },
+    getCategoryInfo() {
+    },
+    changeCategory(e) {
+      if (this.isLoading)
+        return;
+      let selectedIndex = e.target.selectedIndex;
+      let name = selectedIndex === -1 ? '' : e.target.options[selectedIndex].text;
+      this.event.$emit('efm-category.change', this.level, this.selectedCode, name);
+    },
+    onChangeCategory(level, selectedCode) {
+      if (level < this.level) {
+        this.categories = this.categories.slice(0, 1);
+        this.selectedCode = '';
+      }
+    },
+    onLoad(state, level) {
+      this.onPrevLoad();
+      if (level < this.level) {
+        this.isParentLoading = state;
+      }
+    },
+    onPrevLoad() {
+    },
+  }
 }
